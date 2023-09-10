50dc00a6959ff9099687d854102910e688a1ac4a
youngmin13 <wjsdudals56@naver.com>
2023-09-10 19:31:24 +0900

    feat : User 관련 기능들 추가 - UserController - UserDetailsImpl - UserDetailsServiceImpl - UserRepository - UserService - UserServiceImpl

---
+++ src/main/java/com/tnt/ecommeracemarketplace/controller/UserController.java
@@ -0,0 +1,54 @@
+package com.tnt.ecommeracemarketplace.controller;
+
+import com.tnt.ecommeracemarketplace.dto.ApiResponseDto;
+import com.tnt.ecommeracemarketplace.dto.LoginRequestDto;
+import com.tnt.ecommeracemarketplace.dto.SignoutRequestDto;
+import com.tnt.ecommeracemarketplace.dto.SignupRequestDto;
+import com.tnt.ecommeracemarketplace.security.UserDetailsImpl;
+import com.tnt.ecommeracemarketplace.service.UserServiceImpl;
+import jakarta.annotation.Nullable;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.validation.Valid;
+import java.io.IOException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/user")
+public class UserController {
+
+    private final UserServiceImpl userServiceImpl;
+
+    @PostMapping("/login")
+    public ResponseEntity<ApiResponseDto> login(@RequestBody LoginRequestDto loginRequestDto, HttpServletResponse response) {
+        return userServiceImpl.login(loginRequestDto, response);
+    }
+
+    @DeleteMapping("/logout")
+    public ResponseEntity<ApiResponseDto> logout(HttpServletResponse response, Authentication authResult) throws ServletException, IOException {
+
+        return userServiceImpl.logout(response, authResult);
+    }
+
+    @PostMapping("/signup")
+    public ResponseEntity<ApiResponseDto> signup(@Valid @RequestBody SignupRequestDto requestDto) {
+        return userServiceImpl.signup(requestDto);
+    }
+
+    @Transactional
+    @DeleteMapping("/signout")
+    public ResponseEntity<ApiResponseDto> signout(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody SignoutRequestDto signoutRequestDto,
+                                                  HttpServletResponse response, Authentication authResult) throws ServletException, IOException {
+        return userServiceImpl.signout(userDetails.getUser(), signoutRequestDto, response, authResult);
+    }
+}
---
+++ src/main/java/com/tnt/ecommeracemarketplace/security/UserDetailsImpl.java
@@ -0,0 +1,64 @@
+package com.tnt.ecommeracemarketplace.security;
+
+import com.tnt.ecommeracemarketplace.entity.UserRoleEnum;
+import com.tnt.ecommeracemarketplace.entity.Users;
+import java.util.ArrayList;
+import java.util.Collection;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsImpl implements UserDetails {
+
+    private final Users user;
+
+    public UserDetailsImpl(Users user) {
+        this.user = user;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        UserRoleEnum role = user.getRole();
+        String authority = role.getAuthority();
+
+        SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority(authority);
+        Collection<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(simpleGrantedAuthority);
+
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
