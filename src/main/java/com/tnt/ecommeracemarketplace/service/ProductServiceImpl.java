c4f68e8d5f8ce225f66f82061c7f2ddb08d4fd8b
SonMyungJi <linolousky@gmail.com>
2023-09-16 09:44:21 +0900

    feat: 와일드카드/fulltext

--- src/main/java/com/tnt/ecommeracemarketplace/service/ProductServiceImpl.java
+++ src/main/java/com/tnt/ecommeracemarketplace/service/ProductServiceImpl.java
@@ -1,6 +1,5 @@
 package com.tnt.ecommeracemarketplace.service;
 
-import com.tnt.ecommeracemarketplace.dto.ApiResponseDto;
 import com.tnt.ecommeracemarketplace.dto.PageDto;
 import com.tnt.ecommeracemarketplace.dto.ProductListResponseDto;
 import com.tnt.ecommeracemarketplace.dto.ProductResponseDto;
@@ -8,15 +7,14 @@ import com.tnt.ecommeracemarketplace.entity.Orders;
 import com.tnt.ecommeracemarketplace.entity.Products;
 import com.tnt.ecommeracemarketplace.repository.OrderRepository;
 import com.tnt.ecommeracemarketplace.repository.ProductRepository;
-
 import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.cache.annotation.Cacheable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -34,7 +32,7 @@ public class ProductServiceImpl implements ProductService {
 
   private static final Logger logger = LoggerFactory.getLogger(ProductServiceImpl.class);
 
-    // 전체 조회
+  // 전체 조회
   @Override
   public ProductListResponseDto getProducts(PageDto pageDto) {
     Pageable pageable = pageDto.toPageable();
@@ -62,47 +60,89 @@ public class ProductServiceImpl implements ProductService {
     return new ProductResponseDto(productToFind);
   }
 
-    // 상품 주문
-    @Override
-    @Transactional
-    public void orderProducts(Long id, Long quantity) {
+  public ProductListResponseDto findProducts(String keyword, PageDto pageDto) {
+    Pageable pageable = pageDto.toPageable();
+    pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+    Page<Products> productPage = productRepository.findAllByTitle(keyword, pageable);
 
-        if(quantity < 1) throw new IllegalArgumentException("재고 부족");
+    List<ProductResponseDto> productList = productPage.getContent().stream()
+        .map(ProductResponseDto::new)
+        .collect(Collectors.toList());
+
+    return new ProductListResponseDto(productList);
+  }
+
+  // 키워드(LIKE + 와일드카드) 검색
+  public ProductListResponseDto selectProductList(String keyword, PageDto pageDto) {
+    Pageable pageable = pageDto.toPageable();
+    pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+    Page<Products> productPage = productRepository.searchByLike(keyword, pageable);
+
+    List<ProductResponseDto> productList = productPage.getContent().stream()
+        .map(ProductResponseDto::new)
+        .collect(Collectors.toList());
+
+    return new ProductListResponseDto(productList);
+  }
+
+  // 키워드(Full Text) 검색
+//  public ProductListResponseDto selectProductList(String keyword, PageDto pageDto) {
+//    Pageable pageable = pageDto.toPageable();
+//    pageable = PageRequest.of(pageable.getPageNumber(), pageable.getPageSize());
+//    Page<Products> productPage = productRepository.searchByFullText(keyword, pageable);
+//
+//    List<ProductResponseDto> productList = productPage.getContent().stream()
+//        .map(ProductResponseDto::new)
+//        .collect(Collectors.toList());
+//
+//    return new ProductListResponseDto(productList);
+//  }
+
+  // 상품 주문
+  @Override
+  @Transactional
+  public void orderProducts(Long id, Long quantity) {
+
+    if (quantity < 1) {
+      throw new IllegalArgumentException("재고 부족");
+    }
 
 //        Products productTest = productRepository.findById(id).orElseThrow(
 //                () -> new IllegalArgumentException("재고 부족")
 //        );
 
-        Products product = productModify(id, quantity);
+    Products product = productModify(id, quantity);
 
-        // 주문 데이터 저장
-        Orders order = new Orders();
-        order.setAmount(quantity);
-        order.setProduct_price(product.getCost());
-        order.setProducts(product);
-        order.setOrder_date(new Date());
-        order.setTotal_price(product.getCost() * quantity);
+    // 주문 데이터 저장
+    Orders order = new Orders();
+    order.setAmount(quantity);
+    order.setProduct_price(product.getCost());
+    order.setProducts(product);
+    order.setOrder_date(new Date());
+    order.setTotal_price(product.getCost() * quantity);
 
-        orderRepository.save(order);
+    orderRepository.save(order);
+  }
+
+  @Transactional(propagation = Propagation.REQUIRES_NEW)
+  public Products productModify(Long id, Long quantity) {
+
+    Products product = productRepository.findByIdWithPessimisticLock(id);
+
+    logger.info("Current amount (visible to this thread): {}", product.getAmount());
+
+    // 재고 부족 예외처리
+    if (product.getAmount() < quantity) {
+      throw new IllegalArgumentException("재고 부족");
     }
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public Products productModify(Long id, Long quantity){
+    // 상품 재고 차감
+    product.buy(quantity);
 
-        Products product = productRepository.findByIdWithPessimisticLock(id);
+    productRepository.saveAndFlush(product);
 
-        logger.info("Current amount (visible to this thread): {}", product.getAmount());
+    logger.info("buyPessimistic completed successfully for id: {} and quantity: {}", id, quantity);
 
-        // 재고 부족 예외처리
-        if(product.getAmount() < quantity) throw new IllegalArgumentException("재고 부족");
-
-        // 상품 재고 차감
-        product.buy(quantity);
-
-        productRepository.saveAndFlush(product);
-
-        logger.info("buyPessimistic completed successfully for id: {} and quantity: {}", id, quantity);
-
-        return product;
-    }
+    return product;
+  }
 }
