bb14b3701d8d52a0e0b14bee3ddc66719cd712e8
youngmin13 <wjsdudals56@naver.com>
2023-09-16 10:18:30 +0900

    feat : 처음 검색 테스트

--- src/main/java/com/tnt/ecommeracemarketplace/controller/ProductController.java
+++ src/main/java/com/tnt/ecommeracemarketplace/controller/ProductController.java
@@ -52,16 +52,16 @@ public class ProductController {
     return productService.findProductDetails(productId);
   }
 
-//    /**
-//     * 제품 검색 API
-//     *
-//     * @param keyword 검색어
-//     */
-//    @GetMapping("/products/details")
-//    public ProductListResponseDto searchProducts(@RequestParam(name = "page") int page, @RequestParam(name = "keyword") String keyword) {
-//        PageDto pageDto = PageDto.builder().currentPage(page).build();
-//        return productService.selectProductList(keyword, pageDto);
-//    }
+    /**
+     * 제품 검색 API
+     *
+     * @param keyword 검색어
+     */
+    @GetMapping("/products/details")
+    public ProductListResponseDto searchProducts(@RequestParam(name = "page") int page, @RequestParam(name = "keyword") String keyword) {
+        PageDto pageDto = PageDto.builder().currentPage(page).build();
+        return productService.findProducts(keyword, pageDto);
+    }
 
   // Redisson lock 진행해야 하는 코드인데...
   // RDS로 Mysql을 이미 구축해서 일단은 보류
